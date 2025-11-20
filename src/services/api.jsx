e1803ff5da766bb29c299ad259758bfd400e1312
ShafinKhan-Pathan <kshafin575@gmail.com>
2025-11-20 10:38:13 -0600

    Fix the Api issue

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `https://us-central1-api-skinstric-ai.cloudfunctions.net`;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const submitPhaseOne = async (phaseOneData) => {
   try {
